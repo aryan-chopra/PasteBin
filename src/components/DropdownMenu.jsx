65d78f90ba28ecd34f06270e53d6eefb719ca001
aryan-chopra <aryan.chopra1211@gmail.com>
2024-11-27 19:11:22 +0530

    Added visual elements for timer, submit and password

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
-export default function DropdownMenu({ items }) {
-    
+export default function DropdownMenu({ items, heading }) {
+
     const [title, setTitle] = useState(items[0])
 
-    let menuList = items.map((item) => 
+    let menuList = items.map((item) =>
         <Dropdown.Item key={item} onClick={updateTitle} href='#'>{item}</Dropdown.Item>
     )
 
@@ -15,13 +15,17 @@ export default function DropdownMenu({ items }) {
     }
 
     return (
-        <DropdownButton className='w-25' id="dropdown-basic-button" title={title}>
-            {console.log(menuList)}
+        <>
+            <p className="mb-2 fs-5" style={{ display: "inline-block" }}>{heading}</p>
 
-            {menuList}
-            {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <DropdownButton className='w-25' id="dropdown-basic-button" title={title}>
+                {console.log(menuList)}
+
+                {menuList}
+                {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
-        </DropdownButton>
+            </DropdownButton>
+        </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import DropdownMenu from "../components/DropdownMenu";
 import CodingSpace from "../components/CodingSpace";
 
@@ -10,7 +10,19 @@ export default function Home() {
                     <CodingSpace />
                 </Col>
                 <Col>
-                    <DropdownMenu items={["None", "Java", "C", "C++", "GoLang"]}/>
+                    <Form.Control className="w-75 fs-5" placeholder="Title"/>
+
+                    <div className="dropdown-top-margin">
+                        <DropdownMenu heading={"Syntax"} items={["None", "Java", "C", "C++", "GoLang"]} />
+                    </div>
+
+                    <div className="dropdown-top-margin">
+                        <DropdownMenu heading={"Expires after"} items={["Never", "1 second", "1 minute", "1 hour", "1 day", "1 month", "1 year", "1 decade", "1 century", "1 eon", "1 proton life time"]}></DropdownMenu>
+                    </div>
+
+                    <div className="position-fixed bottom-0 end-0 m-3">
+                        <Button variant="success" className="fs-5">Submit</Button>
+                    </div>
                 </Col>
             </Row>
         </Container>
