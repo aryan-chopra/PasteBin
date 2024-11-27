759a5821b89e992bb1632a830dc6d3a9f75680ed
Arjun Pathak <arjunpathak072@gmail.com>
2024-11-27 19:34:51 +0530

    Added backend implementation

---
+++ backend/routes/entityRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import Entity from '../controllers/entityController.js'
+
+const router = express.Router()
+
+router.post('/', Entity.createEntity)
+
+export default router
