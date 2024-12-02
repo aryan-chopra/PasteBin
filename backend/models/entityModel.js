3657ed160185c2899cafc3ceba114058c2b39c57
Arjun Pathak <arjunpathak072@gmail.com>
2024-12-02 22:05:26 +0530

    BE: Refactored the 'entityModel.js' to make validators inline

--- backend/models/entityModel.js
+++ backend/models/entityModel.js
@@ -5,7 +5,10 @@ const entitySchema = new mongoose.Schema({
         type: String,
         required: [true, "Each entity must have a unique url"],
         validate: {
-            validator: urlValidator,
+            validator: async function (url) {
+                const exists = await Entity.findOne({ url: url })
+                return !exists
+            },
             content: "A document already has this url"
         },
         unique: true,
@@ -18,7 +21,9 @@ const entitySchema = new mongoose.Schema({
         type: String,
         required: [true, "Each entity must have a content String"],
         validator: {
-            validate: contentValidator,
+            validate: function (content) {
+                return content !== ""
+            },
             message: "content cannot be empty"
         }
     },
@@ -26,7 +31,9 @@ const entitySchema = new mongoose.Schema({
         type: String,
         required: [true, "Each entity must have a title string"],
         validator: {
-            validate: titleValidator,
+            validate: function (title) {
+                return title !== ""
+            },
             message: "title cannot be empty"
         }
     },
@@ -34,7 +41,10 @@ const entitySchema = new mongoose.Schema({
         type: String,
         required: [true, "Content type must be set to a valid value"],
         validate: {
-            validator: contentTypeValidator,
+            validator: function (contentType) {
+                const validContentTypes = ["Cpp", "Java", "Go", "C", "Text"]
+                return validContentTypes.includes(contentType)
+            },
             message: "Invalid contentType, valid values are: C, Cpp, Go, Java, Text"
         }
     },
@@ -43,32 +53,10 @@ const entitySchema = new mongoose.Schema({
     },
 }, { timestamps: true, collection: "entities" })
 
-// Indexes
-
 entitySchema.index(
     { expiresAfter: 1 },
     { expireAfterSeconds: 0 }
 )
-
-// Custom Validators
-
-function contentValidator(content) {
-    return content !== ""
-}
-
-function titleValidator(title) {
-    return title !== ""
-}
-
-function contentTypeValidator(contentType) {
-    const validContentTypes = ["Cpp", "Java", "Go", "C", "Text"]
-    return validContentTypes.includes(contentType)
-}
-
-async function urlValidator(url) {
-    const exists = await Entity.findOne({ url: url })
-    return !exists
-}
 
 entitySchema.method("toJSON", function () {
     const { __v, _id, ...Object } = this.toObject()
