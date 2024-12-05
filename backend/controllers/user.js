95e5ab0bc7cc4e54dec5840a61a42e1832bd3818
Arjun Pathak <arjunpathak072@gmail.com>
2024-12-05 09:26:27 +0530

    BE: Added a .get method to the User model, along with handling of get requests in the /auth/login route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
-import User from "../models/user.js";
+import bcrypt from "bcrypt";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+import User from "../models/user.js";
 
 User.create = async (request, response) => {
   const { email, username, password } = request.body;
@@ -33,10 +35,40 @@ User.create = async (request, response) => {
       .status(StatusCodes.CREATED)
       .json({ message: "User created successfully" });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     response
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json({ message: ReasonPhrases.INTERNAL_SERVER_ERROR });
+  }
+};
+
+User.authenticate = async (request, response) => {
+  const { username, password } = request.body;
+
+  try {
+    const user = await User.findOne({ username: username }).select("password");
+    
+    if (!user.password) {
+      return response
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Invalid username or password" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return response
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Invalid username or password" });
+    }
+
+    return response
+      .status(StatusCodes.OK)
+      .json({ message: "Authentication successful" });
+  } catch (err) {
+    response
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ message: "Something went wrong" });
+    console.error(err);
   }
 };
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,5 +4,6 @@ import User from "../controllers/user.js";
 const router = express.Router();
 
 router.post("/register", User.create);
+router.get("/login", User.authenticate);
 
 export default router;
