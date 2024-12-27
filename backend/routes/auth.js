a69349be21267596bade9fba246b994ebd0dec63
Arjun Pathak <arjunpathak072@gmail.com>
2024-12-27 14:01:17 +0530

    BE: Changed HTTP method for login to POST

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,6 @@ import User from "../controllers/user.js";
 const router = express.Router();
 
 router.post("/register", User.create);
-router.get("/login", User.authenticate);
+router.post("/login", User.authenticate);
 
 export default router;
